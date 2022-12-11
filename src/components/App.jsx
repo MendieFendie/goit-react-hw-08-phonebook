8d7b206d87e5ee0573a9e112a81f5aacfcddea96
Oleh Savaryn <olegsavaryn@gmail.com>
2022-12-11 12:17:11 +0100

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -45,6 +45,31 @@ export default class App extends React.Component {
     });
   };
 
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+    console.log(localStorage.contacts);
+    if (localStorage.contacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+    if (localStorage.contacts === '[]') {
+      this.setState({
+        contacts: [
+          { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+          { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+          { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+          { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+        ],
+      });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
   render() {
     const normalizedFilter = this.state.filter.toLowerCase();
     const visibleNames = this.state.contacts.filter(contact =>
