48ea0b7e352ff8a624631147a03c8029228a7ab5
Oleh Savaryn <olegsavaryn@gmail.com>
2022-12-30 23:04:43 +0100

    Update ContactForm.jsx

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -56,68 +56,6 @@ export default function ContactForm(props) {
   );
 }
 
-// class oldContactForm extends React.Component {
-//   state = {
-//     name: '',
-//     number: '',
-//   };
-
-//   handleChange = evt => {
-//     this.setState({ [evt.currentTarget.name]: evt.currentTarget.value });
-//     console.log(evt.currentTarget.name);
-//     console.log(evt.currentTarget.value);
-//   };
-
-//   handleSubmit = e => {
-//     e.preventDefault();
-//     this.props.addContact(this.state);
-//     this.resetForm();
-//   };
-
-//   resetForm = () => {
-//     this.setState({ name: '', number: '' });
-//   };
-
-//   render() {
-//     const { name, number } = this.state;
-//     return (
-//       <>
-//         <form className={css.form} onSubmit={this.handleSubmit}>
-//           <label className={css.form_item}>
-//             Name
-//             <input
-//               className={css.form_input}
-//               type="text"
-//               name="name"
-//               value={name}
-//               onChange={this.handleChange}
-//               pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-//               title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-//               required
-//             />
-//           </label>
-//           <label className={css.form_item}>
-//             Number
-//             <input
-//               className={css.form_input}
-//               type="tel"
-//               name="number"
-//               onChange={this.handleChange}
-//               value={number}
-//               pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-//               title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-//               required
-//             />
-//           </label>
-//           <button className={css.form_btn} type="submit">
-//             Add contact
-//           </button>
-//         </form>
-//       </>
-//     );
-//   }
-// }
-
 ContactForm.propTypes = {
   addContact: PropTypes.func.isRequired,
 };
